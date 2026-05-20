3410dbcf72490cec5a9e3f328f9ba0ff8bd1ac60
totomus83 <tom.dibellonio@gmail.com>
2026-05-20 13:32:07 +0200

    Task 0

---
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -0,0 +1,23 @@
+import HbSLogo from './assets/holberton-logo.jpg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='holberton-header'>
+        <img src={HbSLogo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className='holberton-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      <div className='holberton-footer'>
+        <p>Copyright {new Date().getFullYear()} - holberton School</p>
+      </div>
+    </>
+  )
+}
+
+export default App
