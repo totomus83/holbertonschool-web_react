5d2d7f0f9c986e4a22b35a034af7de2e7b51457b
totomus83 <tom.dibellonio@gmail.com>
2026-05-20 14:04:27 +0200

    task 1

--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -2,25 +2,25 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
-  test('Vérification texte h1 App-header', () => {
+  test('Vérification texte h1 holberton-header', () => {
     render(<App />);
     const headerh1 = screen.getByRole('heading', { level: 1, name: /School dashboard/i });
     expect(headerh1).toBeInTheDocument();
   });
 
-  test('Vérification texte App-body', () => {
+  test('Vérification texte holberton-body', () => {
     render(<App />);
     const bodyp = screen.getByText(/Login to access the full dashboard/i);
     expect(bodyp).toBeInTheDocument();
   });
 
-  test('Vérification texte App-footer', () => {
+  test('Vérification texte holberton-footer', () => {
     render(<App />);
     const footerp = screen.getByText(/Copyright \d{4} - holberton School/i);
     expect(footerp).toBeInTheDocument();
   });
 
-  test('Vérification alt image App-header', () => {
+  test('Vérification alt image holberton-header', () => {
     render(<App />);
     const headerImgAlt = screen.getByAltText(/holberton logo/i);
     expect(headerImgAlt).toBeInTheDocument();
