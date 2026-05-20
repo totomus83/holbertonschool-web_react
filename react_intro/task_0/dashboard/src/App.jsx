05baa59f1dc05af3747fc4c71ce71d35b72a4003
totomus83 <tom.dibellonio@gmail.com>
2026-05-20 14:08:42 +0200

    TAsk 1

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -4,20 +4,20 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className='holberton-header'>
+      <div className='App-header'>
         <img src={HbSLogo} alt="holberton logo" />
         <h1>School dashboard</h1>
       </div>
 
-      <div className='holberton-body'>
+      <div className='App-body'>
         <p>Login to access the full dashboard</p>
       </div>
 
-      <div className='holberton-footer'>
-        <p>Copyright {new Date().getFullYear()} - Holberton School</p>
+      <div className='App-footer'>
+        <p>Copyright {new Date().getFullYear()} - holberton School</p>
       </div>
     </>
   )
 }
 
-export default App;
+export default App
--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -2,25 +2,25 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
-  test('Vérification texte h1 holberton-header', () => {
+  test('Vérification texte h1 App-header', () => {
     render(<App />);
     const headerh1 = screen.getByRole('heading', { level: 1, name: /School dashboard/i });
     expect(headerh1).toBeInTheDocument();
   });
 
-  test('Vérification texte holberton-body', () => {
+  test('Vérification texte App-body', () => {
     render(<App />);
     const bodyp = screen.getByText(/Login to access the full dashboard/i);
     expect(bodyp).toBeInTheDocument();
   });
 
-  test('Vérification texte holberton-footer', () => {
+  test('Vérification texte App-footer', () => {
     render(<App />);
     const footerp = screen.getByText(/Copyright \d{4} - holberton School/i);
     expect(footerp).toBeInTheDocument();
   });
 
-  test('Vérification alt image holberton-header', () => {
+  test('Vérification alt image App-header', () => {
     render(<App />);
     const headerImgAlt = screen.getByAltText(/holberton logo/i);
     expect(headerImgAlt).toBeInTheDocument();
