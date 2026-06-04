19cc50ec2bc40d869e8eb3d99072ea9fea80a03a
totomus83 <tom.dibellonio@gmail.com>
2026-06-04 22:06:33 +0200

    Task 0

--- react_styling/task_0/dashboard/src/main.jsx
+++ react_styling/task_0/dashboard/src/main.jsx
@@ -1,21 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App/App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './main.css'
-import App from './App/App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './main.css';
+import App from './App/App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
