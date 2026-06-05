1f86d90de5d9b2e5c4a32f6ed2dd7c95bfa38077
totomus83 <tom.dibellonio@gmail.com>
2026-06-05 19:01:16 +0200

    Task 2

--- react_styling/task_2/dashboard/src/Notifications/NotificationItem.jsx
+++ react_styling/task_2/dashboard/src/Notifications/NotificationItem.jsx
@@ -3,24 +3,26 @@ import { PureComponent } from 'react';
 class NotificationItem extends PureComponent {
   render() {
     const { type = 'default', value, html, markAsRead, id } = this.props;
-    const colorClass = type === 'default'
-      ? 'text-(--default-notification-item)'
-      : 'text-(--urgent-notification-item)';
+
+    const style = {
+      color: type === 'default' ? 'blue' : 'red',
+    };
 
     if (html) {
       return (
         <li
           data-notification-type={type}
-          className={colorClass}
+          style={style}
           dangerouslySetInnerHTML={{ __html: html }}
           onClick={() => markAsRead && markAsRead(id)}
         />
       );
     }
+
     return (
       <li
         data-notification-type={type}
-        className={colorClass}
+        style={style}
         onClick={() => markAsRead && markAsRead(id)}
       >
         {value}
